6c06ed188ca54ddb2a1942c037340f4d1d735ade
Natalya <m_d.r.lecter@protonmail.com>
2022-03-22 23:16:07 +0300

    city and country filters

--- script/index.js
+++ script/index.js
@@ -15,17 +15,28 @@ const resultList = document.querySelector(".result__list");
 const formSearch = document.querySelector(".bottom__search");
 //For header with vacancy
 const found = document.querySelector(".found");
+// for data
+let data;
+// for hidden fields
+const orderBy = document.querySelector("#order_by");
+const searchPeriod = document.querySelector("#search_period");
 
-//getting data from server
-const getData = ({ search, id } = {}) => {
+//getting data from server + country and city choise
+const getData = ({ search, id, country, city } = {}) => {
+  let url = `http://localhost:3000/api/vacancy/${id ? id : ""}`;
   if (search) {
-    return fetch(`http://localhost:3000/api/vacancy?search=${search}`).then(
-      (response) => response.json()
-    );
+    url = `http://localhost:3000/api/vacancy?search=${search}`;
   }
-  return fetch(`http://localhost:3000/api/vacancy/${id ? id : ""}`).then(
-    (response) => response.json()
-  );
+
+  if (city) {
+    url = `http://localhost:3000/api/vacancy?city=${city}`;
+  }
+
+  if (country) {
+    url = `http://localhost:3000/api/vacancy?country=${country}`;
+  }
+
+  return fetch(url).then((response) => response.json());
 };
 
 //fuction returns word and number (for correct ending)
@@ -94,6 +105,29 @@ const renderCards = (data) => {
   resultList.append(...cards);
 };
 
+//function for sorting data
+const sortData = () => {
+  switch (orderBy.value) {
+    case "down":
+      data.sort((a, b) => (a.minCompensation > b.minCompensation ? 1 : -1));
+      break;
+    case "up":
+      data.sort((a, b) => (a.minCompensation < b.minCompensation ? 1 : -1));
+      break;
+    default:
+      data.sort((a, b) =>
+        new Date(a.date).getTime() > new Date(b.date).getTime() ? 1 : -1
+      );
+  }
+};
+
+//function for filtering (all time, month, week etc)
+const filterData = () => {
+  const date = new Date();
+  date.setDate(date.getDate() - searchPeriod.value);
+  return data.filter((item) => new Date(item.date).getTime() > date);
+};
+
 const optionHandler = () => {
   optionBtnOrder.addEventListener("click", () => {
     optionListOrder.classList.toggle("option__list_active");
@@ -111,6 +145,10 @@ const optionHandler = () => {
 
     if (target.classList.contains("option__item")) {
       optionBtnOrder.textContent = target.textContent;
+      //for sorting purposes
+      orderBy.value = target.dataset.sort;
+      sortData();
+      renderCards(data);
       //closes pop-up window
       optionListOrder.classList.remove("option__list_active");
       for (const elem of optionListOrder.querySelectorAll(".option__item")) {
@@ -131,6 +169,10 @@ const optionHandler = () => {
 
     if (target.classList.contains("option__item")) {
       optionBtnPeriod.textContent = target.textContent;
+      //for filtering period (all time, month, week etc)
+      searchPeriod.value = target.dataset.date;
+      const tempData = filterData();
+      renderCards(tempData);
       //closes pop-up window
       optionListPeriod.classList.remove("option__list_active");
       for (const elem of optionListPeriod.querySelectorAll(".option__item")) {
@@ -151,10 +193,19 @@ const cityHandler = () => {
     city.classList.toggle("city_active");
   });
 
-  cityRegionList.addEventListener("click", (event) => {
+  cityRegionList.addEventListener("click", async (event) => {
     const target = event.target;
 
     if (target.classList.contains("city__link")) {
+      // hash output
+      const hash = new URL(target.href).hash.substring(1);
+      const option = {
+        [hash]: target.textContent,
+      };
+      data = await getData(option);
+      sortData();
+      filterData();
+      renderCards(data);
       topCityBtn.textContent = target.textContent;
       //closes pop-up window
       city.classList.remove("city_active");
@@ -294,7 +345,8 @@ const searchHandler = () => {
     if (textSearch.length > 2) {
       formSearch.search.style.borderColor = "";
       //get data from server
-      const data = await getData({ search: textSearch });
+      data = await getData({ search: textSearch });
+      sortData();
       renderCards(data);
 
       found.innerHTML = `${declOfNum(data.length, [
@@ -313,7 +365,9 @@ const searchHandler = () => {
 };
 
 const init = async () => {
-  const data = await getData();
+  data = await getData();
+  sortData();
+  data = filterData();
   renderCards(data);
 
   optionHandler();
